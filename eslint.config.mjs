345c4af0085d09296ad5d50ee713ab6b1f81bd06
mattiss01 <m.scherbacher01@gmail.com>
2026-07-29 11:39:40 +0200

    lint: exclude local worktrees

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,11 @@ import nextTypeScript from "eslint-config-next/typescript";
 export default defineConfig([
   ...nextVitals,
   ...nextTypeScript,
-  globalIgnores([".next/**", "out/**", "coverage/**", "next-env.d.ts"]),
+  globalIgnores([
+    ".next/**",
+    ".worktrees/**",
+    "out/**",
+    "coverage/**",
+    "next-env.d.ts",
+  ]),
 ]);
